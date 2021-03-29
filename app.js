0d15c292fae6bf3e5e300fd39a7b36c202f602c3
Po <s5819874@hotmail.com>
2021-03-29 11:47:39 +0800

    feat: install express-session and connect-flash to show messages

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const app = express()
 const db = require('./models')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'YouWillNeverKnow',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
+require('./routes')(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-require('./routes')(app)
 
 module.exports = app
