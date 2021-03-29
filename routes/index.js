d975323878c06d16a7a503f8382210f6d5e375a7
Po <s5819874@hotmail.com>
2021-03-29 09:38:15 +0800

    feat: project init and add restaurant index page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
+const restController = require('../controllers/restController')
+
 module.exports = (app) => {
 
   app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.redirect('/restaurants')
   })
+
+  app.get('/restaurants', restController.getRestaurants)
+
 }
