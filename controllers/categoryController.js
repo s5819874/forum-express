2b4c18704c3076f004a8e89420aa077d73f35805
Po <s5819874@hotmail.com>
2021-04-05 15:50:01 +0800

    add admin create category feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,6 +8,15 @@ let categoryController = {
     }).then(categories => {
       return res.render('admin/categories', { categories })
     })
+  },
+  postCategory: (req, res) => {
+    const { name } = req.body
+    if (!name) {
+      req.flash('warning_msg', '請輸入名稱')
+      return res.redirect('back')
+    }
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
   }
 }
 module.exports = categoryController
