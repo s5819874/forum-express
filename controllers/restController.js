ce746c58748a3da014ee8284fff711c256fb0b47
POYI LEE <s5819874@hotmail.com>
2021-04-06 10:30:54 +0800

    add pagination on restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,18 +1,35 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 const restController = {
   getRestaurants: (req, res) => {
     const whereQuery = {}
     let categoryId = ''
+    let offset = 0
+    if (req.query.page) {
+      offset = (Number(req.query.page) - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
     }
-    Restaurant.findAll({ include: [Category], where: whereQuery })
+    Restaurant.findAndCountAll({
+      include: [Category],
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
+    })
       .then(restaurants => {
-        const data = restaurants.map(r => ({
+        //page settings
+        const page = Number(req.query.page) | 1
+        const maxPage = Math.ceil(restaurants.count / pageLimit)
+        const pageList = Array.from({ lengh: maxPage }).map((item, index) => index + 1)
+        const prev = page - 1 < 1 ? 1 : page - 1
+        const next = page + 1 > maxPage ? maxPage : page + 1
+
+        const data = restaurants.rows.map(r => ({
           ...r.dataValues,
           description: r.description.substring(0, 50),
           categoryName: r.Category.name,
@@ -20,7 +37,15 @@ const restController = {
         }))
         Category.findAll({ raw: true, nest: true })
           .then(categories => {
-            return res.render('restaurants', { restaurants: data, categories, categoryId })
+            return res.render('restaurants', {
+              restaurants: data,
+              categories,
+              categoryId,
+              page,
+              pageList,
+              prev,
+              next
+            })
           })
       })
       .catch(err => res.send(err))
