ed9175119a278f2ab204bca97f02d04115ef9975
POYI LEE <s5819874@hotmail.com>
2021-04-06 14:34:02 +0800

    add user update profile feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const userController = {
   signUpPage: (req, res) => {
@@ -57,6 +59,46 @@ const userController = {
         res.render('editProfile', { user: user.toJSON() })
       })
       .catch(err => res.send(err))
+  },
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "名字不見了?")
+      return res.redirect('back')
+    }
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        if (err) console.log(err)
+
+        return User.findByPk(req.params.id)
+          .then(user => {
+            user.update({
+              name: req.body.name,
+              email: req.body.email,
+              image: img.data.link
+            })
+              .then((user) => {
+                req.flash('success_msg', 'Your profile was successfully updated')
+                res.redirect(`/users/${user.id}`)
+              })
+          })
+          .catch(err => res.send(err))
+      })
+    } else {
+      return User.findByPk(req.params.id)
+        .then(user => {
+          user.update({
+            name: req.body.name,
+            email: req.body.email,
+          })
+            .then(user => {
+              req.flash('success_msg', 'Your profile was successfully updated')
+              res.redirect(`/users/${user.id}`)
+            })
+        })
+        .catch(err => res.send(err))
+    }
   }
 }
 
