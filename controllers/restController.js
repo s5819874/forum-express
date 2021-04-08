69c40462c9506678f1a8c76a288eccaf3bfa273e
Po <s5819874@hotmail.com>
2021-04-08 22:21:52 +0800

    show restaurant viewCounts on dashboard page

--- controllers/restController.js
+++ controllers/restController.js
@@ -60,10 +60,12 @@ const restController = {
       ]
     })
       .then(restaurant => {
-        console.log(restaurant)
-        return res.render('restaurant', {
-          restaurant: restaurant.toJSON()
-        })
+        return restaurant.increment('viewCounts', { by: 1 })
+          .then(() => {
+            return res.render('restaurant', {
+              restaurant: restaurant.toJSON()
+            })
+          })
       })
       .catch(err => res.send(err))
   },
